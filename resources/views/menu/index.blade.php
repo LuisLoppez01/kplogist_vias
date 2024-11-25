09ed7261f3ba68f302a562c87a3b39d5c0d90da5
ferchoel <lufeesle@gmail.com>
2024-11-25 14:08:02 -0600

    Update data visualization with charts

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -21,7 +21,7 @@
             <div class="card-body">
                 <div class="row">
                     <!-- Fila 1: Gráficas de Tracksections -->
-                    <div class="card col-md-6">
+                    <div class="card col-md-4">
                         <h5 class="card-header">Avance de inspeccón de Vias</h5>
                         <div class="card-body">
                             <div >
@@ -31,13 +31,27 @@
                             </div>
                         </div>
                     </div>
-                    <div class="card col-md-6">
+                    <div class="card col-md-4">
                         <h5 class="card-header">Inpecciones de vias</h5>
                         <div class="card-body">
                             <div >   
                                 <div class="chart-container">
                                     <canvas id="tracksectionsConditionChart_{{ $yard['yard_id'] }}"></canvas>
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card col-md-4">
+                        <h5 class="card-header">Defectos en inspecciones BO</h5>
+                        <div class="card-body">
+                            <div >  
+                                <div class="chart-container">
+                                    <canvas id="tracksectionsPieChartDefects_{{ $yard['yard_id'] }}"></canvas>
+                                </div> 
+                                {{-- <p>{{ $yard['yard_name'] }}</p>
+                                <p>
+                                    <pre>{{ json_encode($yard, JSON_PRETTY_PRINT) }}</pre>
+                                </p> --}}
                             </div>
                         </div>
                     </div>
@@ -48,7 +62,7 @@
                 <div class="row">
 
                     <!-- Fila 2: Gráficas de RailroadSwitches -->
-                    <div class="card col-md-6">
+                    <div class="card col-md-4">
                         <h5 class="card-header">Avance de inspeccón de Herrajes</h5>
                         <div class="card-body">
                             <div >
@@ -59,12 +73,22 @@
                         </div>
                     </div>
 
-                    <div class="card col-md-6">
+                    <div class="card col-md-4">
                         <h5 class="card-header">Inpecciones de Herrajes</h5>
                         <div class="card-body">
                             <div >
                                 <div class="chart-container">
                                     <canvas id="railroadSwitchesConditionChart_{{ $yard['yard_id'] }}"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card col-md-4">
+                        <h5 class="card-header">Defectos en inspecciones BO</h5>
+                        <div class="card-body">
+                            <div >
+                                <div class="chart-container">
+                                    <canvas id="railroadSwitchesPieChartDefects_{{ $yard['yard_id'] }}"></canvas>
                                 </div>
                             </div>
                         </div>
@@ -95,6 +119,7 @@
 
 @section('js')
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             @foreach($yardsData as $yard)
@@ -104,7 +129,14 @@
                     without_inspection: {{ $yard['tracksections']['without_inspection'] }},
                     condition_0: {{ $yard['tracksections']['condition_0'] }},
                     condition_1: {{ $yard['tracksections']['condition_1'] }},
+                    priority_1: {{ $yard['tracksections']['defects_priority_counts']['priority_1']}},
+                    priority_2: {{ $yard['tracksections']['defects_priority_counts']['priority_2']}},
+                    priority_3: {{ $yard['tracksections']['defects_priority_counts']['priority_3']}},
+
                 };
+
+                console.log('trackSection'+ tracksectionsData.priority_3);
+                
 
                 // Gráfico de Dona para Tracksections
                 var ctxTracksections = document.getElementById('tracksectionsPieChart_{{ $yard['yard_id'] }}').getContext('2d');
@@ -132,7 +164,20 @@
                     options: {
                         responsive: true,
                         maintainAspectRatio: false,
-                    }
+                        plugins: {
+                            datalabels: {
+                                color: '#fff',
+                                font: {
+                                    size: 14,
+                                    weight: 'bold'
+                                },
+                                formatter: (value) => value,
+                                align: 'center',
+                                anchor: 'center'
+                            }
+                        }
+                    },
+                    plugins: [ChartDataLabels]  
                 });
 
                 // Gráfico de barras para condiciones de Tracksections
@@ -156,13 +201,67 @@
                     }
                 });
 
+                // Gráfico de Dona para Tracksections
+                var ctxTracksections = document.getElementById('tracksectionsPieChartDefects_{{ $yard['yard_id'] }}').getContext('2d');
+                new Chart(ctxTracksections, {
+                    type: 'doughnut',
+                    data: {
+                    
+                        labels: ['Prioridad Baja', 'Prioridad Media', 'Prioridad Alta'],
+                        datasets: [{
+                            label: 'Tracksections',
+                            data: [
+                                tracksectionsData.priority_1,
+                                tracksectionsData.priority_2,
+                                tracksectionsData.priority_3
+                            ],
+                            backgroundColor: [
+                                'rgba(75, 192, 192, 0.7)',
+                                'rgba(255, 206, 86, 0.7)',
+                                'rgba(255, 99, 132, 0.7)',// Color
+                            ],
+                            borderColor: [
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(255, 99, 132, 1)',// Borde para Sin Inspección
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        plugins: {
+                            datalabels: {
+                                color: '#fff',
+                                font: {
+                                    size: 14,
+                                    weight: 'bold'
+                                },
+                                formatter: (value) => value,
+                                align: 'center',
+                                anchor: 'center'
+                            }
+                        }
+                    },
+                    plugins: [ChartDataLabels]                    
+                });
+
+                
+
                 // Datos para RailroadSwitches
                 var railroadSwitchesData = {
                     total: {{ $yard['railroadSwitches']['total'] }},
                     without_inspection: {{ $yard['railroadSwitches']['without_inspection'] }},
                     condition_0: {{ $yard['railroadSwitches']['condition_0'] }},
                     condition_1: {{ $yard['railroadSwitches']['condition_1'] }},
+                    priority_1:  {{ $yard['railroadSwitches']['defects_priority_counts']['priority_1']}},
+                    priority_2:  {{ $yard['railroadSwitches']['defects_priority_counts']['priority_2']}},
+                    priority_3:  {{ $yard['railroadSwitches']['defects_priority_counts']['priority_3']}},
+
                 };
+
+                console.log('RAilSwitch'+railroadSwitchesData.priority_3);
 
                 // Gráfico de Dona para RailroadSwitches
                 var ctxRailroadSwitches = document.getElementById('railroadSwitchesPieChart_{{ $yard['yard_id'] }}').getContext('2d');
@@ -190,7 +289,20 @@
                     options: {
                         responsive: true,
                         maintainAspectRatio: false,
-                    }
+                        plugins: {
+                            datalabels: {
+                                color: '#fff',
+                                font: {
+                                    size: 14,
+                                    weight: 'bold'
+                                },
+                                formatter: (value) => value,
+                                align: 'center',
+                                anchor: 'center'
+                            }
+                        }
+                    },
+                    plugins: [ChartDataLabels]  
                 });
 
                 // Gráfico de barras para condiciones de RailroadSwitches
@@ -213,6 +325,58 @@
                         maintainAspectRatio: false,
                     }
                 });
+
+                // Gráfico de Dona para railroadSwitches
+                var ctxRailroadSwitches = document.getElementById('railroadSwitchesPieChartDefects_{{ $yard['yard_id'] }}').getContext('2d');
+                new Chart(ctxRailroadSwitches, {
+                    type: 'doughnut',
+                    data: {
+                    
+                        labels: ['Prioridad Baja', 'Prioridad Media', 'Prioridad Alta'],
+                        datasets: [{
+                            label: 'RailroadSwitches',
+                            data: [
+                                railroadSwitchesData.priority_1,
+                                railroadSwitchesData.priority_2,
+                                railroadSwitchesData.priority_3
+                            ],
+                            backgroundColor: [
+                                'rgba(75, 192, 192, 0.7)',
+                                'rgba(255, 206, 86, 0.7)',
+                                'rgba(255, 99, 132, 0.7)',// Color
+                            ],
+                            borderColor: [
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(255, 99, 132, 1)',// Borde para Sin Inspección
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        plugins: {
+                            datalabels: {
+                                color: '#fff',
+                                font: {
+                                    size: 14,
+                                    weight: 'bold'
+                                },
+                                formatter: (value) => value,
+                                align: 'center',
+                                anchor: 'center'
+                            }
+                        }
+                    },
+                    plugins: [ChartDataLabels]                    
+                });
+
+
+
+
+
+
             @endforeach
         });
     </script>
